e1ad855192d5a6ecc342a2a8b2ce344541d7f53d
d3vaa <90439029+d3vaa@users.noreply.github.com>
2021-09-16 14:26:10 -0700

    Create DevaNarayanan.java

---
+++ DevaNarayanan.java
@@ -0,0 +1,24 @@
+ Public class DevaNarayanan
+ {
+    public static void main(String[] args)
+       {
+               System.out.println("Hello Guys!");
+               System.out.println("My name is Deva Narayanan.");
+               System.out.println("I'm from Kerala, India but settled in Dubai.\n Completed most of my studies in Dubai");
+               System.out.println("I am very much intrested in game development and that is the reason why I chose computing science as my Bachelors degree");
+               System.out.println("Moreover I like cycling, swimming and reading books");
+               System.out.println("My initals are:");
+               System.out.println(".----------------.  .-----------------.\n" +
+                                  "| .--------------. || .--------------. |\n" +
+                                  "| |  ________    | || | ____  _____  | |\n" +
+                                  "| | |_   ___ `.  | || ||_   \\|_   _| | |\n" +
+                                  "| |   | |   `. \\ | || |  |   \\ | |   | |\n" +
+                                  "| |   | |    | | | || |  | |\\ \\| |   | |\n" +
+                                  "| |  _| |___.' / | || | _| |_\\   |_  | |\n" +
+                                  "| | |________.'  | || ||_____|\\____| | |\n" +
+                                  "| |              | || |              | |\n" +
+                                  "| '--------------' || '--------------' |\n"+
+                                  " '----------------'  '----------------' ");
+
+   }
+}
